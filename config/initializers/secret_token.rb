ab450322f8fb45654497bbf74fb70eb657a17e6f
Timo Kohorst <timo-hb@arcor.de>
2012-11-07 19:05:05 +0100

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SolarWash::Application.config.secret_token = '9acdc13dae5969bfb2f632a10e11a0fb3d97b72ee3bf21bd173e34f3cf1cf0aa1b85ab6f34772ae52710a8e859f03d7e19fb212d77ba3ab972cce40b8385a3fb'
