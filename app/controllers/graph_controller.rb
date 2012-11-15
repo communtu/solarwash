bbecbae55a4a2e72a271e855859d380982fe2aae
Timo Kohorst <timo-hb@arcor.de>
2012-11-15 11:37:30 +0100

    first graph sample

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -7,24 +7,22 @@ class GraphController < ApplicationController
   #   image_tag graph_url(:action => 'show', :id => 42)
 
   def show
-    g = Gruff::Line.new
+    g = Gruff::StackedArea.new
     # Uncomment to use your own theme or font
     # See http://colourlovers.com or http://www.firewheeldesign.com/widgets/ for color ideas
-#     g.theme = {
-#       :colors => ['#663366', '#cccc99', '#cc6633', '#cc9966', '#99cc99'],
-#       :marker_color => 'white',
-#       :background_colors => ['black', '#333333']
-#     }
+     g.theme = {
+       :colors => ['red', 'yellow', 'blue', '#cc9966', '#99cc99'],
+       :marker_color => 'black',
+       :background_colors => ['white', 'white']
+     }
 #     g.font = File.expand_path('artwork/fonts/VeraBd.ttf', RAILS_ROOT)
 
     g.title = "Gruff-o-Rama"
     
-    g.data("Apples", [1, 2, 3, 4, 4, 3])
-    g.data("Oranges", [4, 8, 7, 9, 8, 9])
-    g.data("Watermelon", [2, 3, 1, 5, 6, 8])
-    g.data("Peaches", [9, 9, 10, 8, 7, 9])
+    g.data("Produktion", [1, 2, 3, 4, 4, 4, 4, 5, 5, 5, 4, 5, 6, 7, 6, 6, 5, 5, 5, 4, 4, 4, 4, 3])
+    g.data("Verbrauch", [0, 1, 2, 3, 3, 3, 3, 4, 4, 4, 3, 4, 5, 6, 5, 5, 4, 4, 4, 3, 3, 3, 3, 2])
 
-    g.labels = {0 => '2004', 2 => '2005', 4 => '2006'}
+    g.labels = {0 => '00:00', 6 => '06:00', 12 => '12:00', 18 => '18:00', 24 => '00:00'}
 
     send_data(g.to_blob, :disposition => 'inline', :type => 'image/png', :filename => "gruff.png")
   end
