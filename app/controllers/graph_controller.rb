a9442c8fc8b34fc98a56acf225b167c5b6f779fe
Till Mossakowski <till@communtu.de>
2012-11-19 18:59:25 +0100

    better colors for graph

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -7,11 +7,11 @@ class GraphController < ApplicationController
   #   image_tag graph_url(:action => 'show', :id => 42)
 
   def show
-    g = Gruff::StackedArea.new
+    g = Gruff::Area.new
     # Uncomment to use your own theme or font
     # See http://colourlovers.com or http://www.firewheeldesign.com/widgets/ for color ideas
      g.theme = {
-       :colors => ['red', 'yellow', 'blue', '#cc9966', '#99cc99'],
+       :colors => ['red','#8D440B', '#ffcc01',   '#cc9966', '#99cc99'],
        :marker_color => 'black',
        :background_colors => ['white', 'white']
      }
@@ -19,8 +19,15 @@ class GraphController < ApplicationController
 
     #g.title = "Gruff-o-Rama"
     
-    g.data("Verbrauch", [2+rand(1), 2+rand(2), 2+rand(3), 2+rand(4), 2+rand(4), 2+rand(4), 2+rand(4), 2+rand(5), 2+rand(5), 2+rand(5), 2+rand(4), 2+rand(5), 2+rand(6), 2+rand(7), 2+rand(6), 2+rand(6), 2+rand(5), 2+rand(5), 2+rand(5), 2+rand(4), 2+rand(4), 2+rand(4), 2+rand(4), 2+rand(3)])
-    g.data("Produktion", [2+rand(0), 2+rand(1), 2+rand(2), 2+rand(3), 2+rand(3), 2+rand(3), 2+rand(3), 2+rand(4), 2+rand(4), 2+rand(4), 2+rand(3), 2+rand(4), 2+rand(5), 2+rand(6), 2+rand(5), 2+rand(5), 2+rand(4), 2+rand(4), 2+rand(4), 2+rand(3), 2+rand(3), 2+rand(3), 2+rand(3), 2+rand(2)])
+    v = [2+rand(1), 2+rand(2), 2+rand(3), 2+rand(4), 2+rand(4), 2+rand(4), 2+rand(4), 2+rand(5), 2+rand(5), 2+rand(5), 2+rand(4), 2+rand(5), 2+rand(6), 2+rand(7), 2+rand(6), 2+rand(6), 2+rand(5), 2+rand(5), 2+rand(5), 2+rand(4), 2+rand(4), 2+rand(4), 2+rand(4), 2+rand(3)]
+    p = [0, 0,1, 1+rand(0), 1+rand(1), 2+rand(3), 3+rand(3), 3+rand(4), 4+rand(4), 4+rand(4), 3+rand(3), 2+rand(4), 2+rand(5), 2+rand(6), 2+rand(5), 2+rand(5), 2+rand(4), 2+rand(4), 2+rand(4), 2+rand(3), 2+rand(3), 0+rand(0), 1, 0]
+    e = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0]
+    (0..23).each do |i|
+      e[i] = [v[i],p[i]].min
+    end
+    g.data("Verbrauch", v)
+    g.data("solarer Verbrauch", e)
+    g.data("Produktion", p)
 
     g.labels = {0 => '00:00', 6 => '06:00', 12 => '12:00', 18 => '18:00', 24 => '00:00'}
 
