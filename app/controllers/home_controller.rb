909a41240c4eccb3783e297af500bdb75b178985
Till Mossakowski <till@communtu.de>
2012-12-03 19:06:06 +0100

    transparent background image

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,2 +1,6 @@
 class HomeController < ApplicationController
+  def home  
+    files = ["Drizzle.png","Mostly Cloudy.png","Slight Drizzle.png","Thunderstorms.png","Cloudy.png","Haze.png","Snow.png","Sunny.png"]
+    @weather = files[rand(files.length)]
+  end  
 end
