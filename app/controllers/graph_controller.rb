404401f9ef0055ee3d2c715f598f221d536e79f5
Timo Kohorst <timo-hb@arcor.de>
2012-12-06 11:38:58 +0100

    begun to implement database read

--- app/controllers/graph_controller.rb
+++ app/controllers/graph_controller.rb
@@ -17,14 +17,16 @@ class GraphController < ApplicationController
        :background_colors => ['white', 'white']
      }
 #    g.font = File.expand_path('artwork/fonts/VeraBd.ttf', RAILS_ROOT)
-
-    wash = [1,23,51,31,1093,1916,1919,1912,1922,1905,1912,1906,1915,1900,1913,1903,1910,1901,1909,1896,1902,1897,1906,1899,1903,1893,861,61,1170,2009,2005,2006,2005,2001,1995,1998,1999,1995,2000,2003,2006,1999,912,60,69,60,129,176,174,173,117,62,48,20,21,141,168,170,167,85,57,29,19,45,173,171,170,154,60,47,4,42,171,170,169,161,62,200,674,539,117,115,170,169,123,59,309,775,776,777,770,762,245]
-    season = 6+rand(6)
-    v = wash*15 + [0]*45
-    p = (-720..719).map {|x|  [0,3000-5*x*x*season*season/864/10].max}
-    s= []
-    (0..1439).each do |i|
-      s<< [v[i],p[i]].min
+    if session[:start].nil? then 
+      session[:start] = Date.today.to_time+21600 # set start time to today, 6am
+      session[:scale] = 720*60
+    end
+    stop = session[:start]+session[:scale]
+    wash = Energylog.where("time >= :start and time <= :stop",
+    {:start => session[:start], :stop => stop})
+    v = 
+    p = 
+    s = wash.map
     end
     g.data("externer Verbrauch", v)
     g.data("solarer Verbrauch", s)
